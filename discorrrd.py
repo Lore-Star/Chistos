fc450b0d6ebcd44d40bd21dae5d2595970398111
Lore <85777582+Lore-Star@users.noreply.github.com>
2024-05-10 19:10:57 -0500

    Add files via upload

--- discorrrd.py
+++ discorrrd.py
@@ -1,4 +1,5 @@
 import discord
+import random
 from discord.ext import commands
 from config import token
 
@@ -8,13 +9,18 @@ intents.message_content = True
 
 bot = commands.Bot(command_prefix='c!', intents=intents)
 
+class Slapper(commands.Converter):
+    async def convert(self, ctx, argument):
+        to_slap = random.choice(ctx.guild.members)
+        return f'{ctx.author} **se papeo a** {argument}'
+
 @bot.event
 async def on_ready():
     print(f'We have logged in as {bot.user}')
 
 @bot.command()
-async def hello(ctx):
-    await ctx.send(f'Hola, soy un bot {bot.user}!')
+async def papear(ctx, *, reason: Slapper):
+    await ctx.send(reason)
 
 @bot.command()
 async def pe(ctx, count_heh = 5):
